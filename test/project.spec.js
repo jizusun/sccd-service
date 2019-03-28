384690d0fd2d681eb8e3a3f3f40210ebdca51e57
Xiaocheng Wei <xiaocheng.wei@sap.com>
2019-03-28 14:37:10 +0100

    Add test for Project object

--- test/project.spec.js
+++ test/project.spec.js
@@ -15,11 +15,25 @@ describe("UI5 Project Testing", () => {
     });
 
     it("should get the correct project id 'sap.b1.smp.pum'", () => {
-    	oProject.getProjectId().then((sId) => {
+    	return oProject.getProjectId().then((sId) => {
     		expect(sId).toEqual("sap.b1.smp.pum");
     	});
-    	/*const sId = await oProject.getProjectId();
-    	expect(sId).toEqual("sap.b1.smp.pum");*/
+    });
+
+    it("should get correct test kpi", () => {
+    	return oProject.getTestKpi().then((oKpi) => {
+    		expect(oKpi[Project.TestType.Unit]).toEqual({ passed: 226, failed: 0, skipped: 0, assertion: 544 });
+    		expect(oKpi[Project.TestType.Integration]).toEqual({ passed: 32, failed: 2, skipped: 0, assertion: 39 });
+    	});
+    });
+
+    it("should get correct unit test coverage", () => {
+        return oProject.getUTCoverage().then((oCoverage) => {
+            expect(oCoverage).toEqual({
+                Included: { validLines: 858, lineRate: '0.92' },
+                All: { validLines: 2819, lineRate: '0.28' }
+            });
+        });
     });
 });
 
@@ -28,7 +42,7 @@ describe("ABAP Project Testing", () => {
 	beforeAll(() => {
 		oProject = new Project({
 			workSpace: "./data/workspace/BCD_ABAP_UT",
-			projectId: "sap.b1.smp.pum"
+			projectId: "sap.support.bcd"
 		});
 	});
 
@@ -36,4 +50,24 @@ describe("ABAP Project Testing", () => {
         expect(oProject.getProjectType()).toEqual(Project.Type.BackEnd);
     });
 
+    it("should get the correct project id 'sap.b1.smp.pum'", () => {
+        return oProject.getProjectId().then((sId) => {
+            expect(sId).toEqual("sap.support.bcd");
+        });
+    });
+
+    it("should get correct test kpi", () => {
+        return oProject.getTestKpi().then((oKpi) => {
+            expect(oKpi[Project.TestType.Unit]).toEqual({ passed: 1034, failed: 3, skipped: 0, assertion: 1037 });
+        });
+    });
+
+    it("should get correct unit test coverage", () => {
+        return oProject.getUTCoverage().then((oCoverage) => {
+            expect(oCoverage).toEqual({
+                Included: { validLines: 12753, lineRate: '0.61' },
+                All: { validLines: 0, lineRate: 0 }
+            });
+        });
+    });
 });
